b7762dab31998735c95523125a9281b5182d5423
ExoHeart <69839114+ExoHeart@users.noreply.github.com>
2024-02-29 14:48:00 +0800

    FINISHED SONG YAY

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners = new ArrayList<>();
+
+    public Song(String title, String artist)
+    {
+        this.title = title;
+        this.artist = artist;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getArtist()
+    {
+        return artist;
+    }
+    public int howMany(ArrayList<String> names)
+    {
+        int newListeners = 0;
+
+        for (String listener : names) {
+            String lowercaseListener = listener.toLowerCase();
+
+            if (!this.listeners.contains(listener.toLowerCase())) {
+                listeners.add(lowercaseListener);
+                newListeners++;
+            }
+        }
+
+        return newListeners;
+    }
 }
